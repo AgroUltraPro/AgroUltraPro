75b329a65aec639c75cf0d595e6897f0c3eb1f4a
adityamotwani <72966970+adityamotwani@users.noreply.github.com>
2021-04-01 20:35:46 +0530

    Model modified
    Removed field crop in farmer and farmer field in notification, and added farmer field in crop and crop field in notification.

--- agropro/home/models.py
+++ agropro/home/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-class Crop(models.Model):
-    name=models.CharField(max_length=150)
-    quantity=models.IntegerField()
-    available=models.BooleanField()
-    price=models.FloatField()
-
-    def __str__(self):
-        return self.name
-
 class Farmer(models.Model):
     name=models.CharField(max_length=150)
     phone=models.CharField(max_length=10)
@@ -19,7 +9,7 @@ class Farmer(models.Model):
     email=models.EmailField(null=True, blank=True)
     area=models.IntegerField(null=True, blank=True)
     address=models.CharField(max_length=300,null=True, blank=True)
-    crop=models.ManyToManyField(Crop)
+    #crop=models.ManyToManyField(Crop)
 
     def __str__(self):
         return self.name
@@ -34,12 +24,20 @@ class Wholesaler(models.Model):
     def __str__(self):
         return self.name
 
+class Crop(models.Model):
+    name=models.CharField(max_length=150)
+    quantity=models.IntegerField()
+    available=models.BooleanField()
+    price=models.FloatField()
+    farmer=models.ForeignKey(Farmer,on_delete=models.CASCADE)
+    
 
+    def __str__(self):
+        return self.name
     
 class Notification(models.Model):
-    farmer=models.ManyToManyField(Farmer)
-    wholesaler=models.ManyToManyField(Wholesaler)
+    crop=models.ForeignKey(Crop,on_delete=models.CASCADE)
+    wholesaler=models.ForeignKey(Wholesaler,on_delete=models.CASCADE)
     accepted=models.BooleanField()
     interested=models.BooleanField()
 
-    
